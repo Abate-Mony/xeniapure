f5a5d6b00ddc7587ef6b9a6709266c845d0fdf4e
Ako Bate Emmanuel <95502815+Abate-Mony@users.noreply.github.com>
2024-10-31 20:57:02 +0000

    rm pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,36 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [react(),
-  VitePWA({
-    manifest: {
-      short_name: "ShipSharpCleaning",
-      name: "ShipSharp Cleaning Services",
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#317EFB',
-      icons: [
-        {
-          "src": "/main-logo.png",
-          "sizes": "64x64",
-          "type": "image/png"
-        },
-        {
-          "src": "/main-logo.png",
-          "sizes": "192x192",
-          "type": "image/png"
-        },
-        {
-          "src": "/main-logo.png",
-          "sizes": "512x512",
-          "type": "image/png"
-        }
-      ],
-    }
-  })
+  // VitePWA({
+  //   manifest: {
+  //     short_name: "ShipSharpCleaning",
+  //     name: "ShipSharp Cleaning Services",
+  //     start_url: '/',
+  //     display: 'standalone',
+  //     background_color: '#ffffff',
+  //     theme_color: '#317EFB',
+  //     icons: [
+  //       {
+  //         "src": "/main-logo.png",
+  //         "sizes": "64x64",
+  //         "type": "image/png"
+  //       },
+  //       {
+  //         "src": "/main-logo.png",
+  //         "sizes": "192x192",
+  //         "type": "image/png"
+  //       },
+  //       {
+  //         "src": "/main-logo.png",
+  //         "sizes": "512x512",
+  //         "type": "image/png"
+  //       }
+  //     ],
+  //   }
+  // })
 
   ],
   resolve: {
